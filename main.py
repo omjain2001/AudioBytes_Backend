a9226dc5de12a6cc2308a9eb02b5331165e8a98b
Akash-213 <akash213kulkarni@gmail.com>
2023-01-27 13:11:05 +0530

    upload audio done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, redirect, jsonify
 import os
 import urllib.request
 from werkzeug.utils import secure_filename
@@ -6,6 +6,11 @@ import whisper
 import numpy as np
 import io
 import soundfile as sf
+import speech_recognition as sr
+from tempfile import NamedTemporaryFile
+
+# Load the Whisper model:
+model = whisper.load_model('base')
 
 app = Flask(__name__)
 app.secret_key = "caircocoders-ednalan"
@@ -23,6 +28,38 @@ def allowed_file(filename):
 @app.route('/')
 def main():
     return "Is this Working ?"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    transcript = ""
+    transcripts = []
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No File Uploaded"
+
+        file = request.files["file"]
+        if file.filename == "":
+            return "Filename cant be empty"
+
+        print("Form Data Received !!")
+        if file :
+            uploads_dict = request.files.to_dict()
+            print("Items : ",uploads_dict.items())
+
+            for fileName, fileStorage in uploads_dict.items():
+                temp = NamedTemporaryFile()
+                fileStorage.save(temp)
+                result = model.transcribe(temp.name)
+                transcripts.append(result)
+
+        # if file:
+        #     recognizer = sr.Recognizer()
+        #     audioFile = sr.AudioFile(file)
+        #     with audioFile as source:
+        #         data = recognizer.record(source)
+        #     transcript = recognizer.recognize_google(data, key=None)
+
+    return transcripts
 
 
 @app.route('/timestamps', methods=['POST'])
@@ -54,50 +91,6 @@ def getTimestamps():
             timestamps.append([start_time, end_time])
 
     return timestamps
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
-        resp.status_code = 400
-        return resp
-
-    file = request.files['file']
-    errors = {}
-    success = True
-    if file and allowed_file(file.filename):
-        # filename = secure_filename(file.filename)
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # file_address = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-        # converting auido to text using whisper
-        data, sample_rate = sf.read(io.BytesIO(file.read()))
-        model = whisper.load_model("base")
-        transcript = model.transcribe(
-            np.float32(data), fp16=False)
-        success = True
-        print(transcript)
-
-    else:
-        errors[file.filename] = 'File type is not allowed'
-
-    if success and errors:
-        errors['message'] = 'File(s) successfully uploaded'
-        resp = jsonify(errors)
-        resp.status_code = 500
-        return resp
-
-    if success:
-        # resp = jsonify({'message' : 'Files successfully uploaded'})
-        resp = jsonify(transcript)
-        resp.status_code = 201
-        return resp
-    else:
-        resp = jsonify(errors)
-        resp.status_code = 500
-        return resp
 
 
 if __name__ == '__main__':
