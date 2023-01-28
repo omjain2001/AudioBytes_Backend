4ccc12ec273c4cddfa6bbb8f289423dae2a98737
Om Jain <omjain2606@gmail.com>
2023-01-28 14:33:14 +0530

    Added CORS

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import io
 import soundfile as sf
 import speech_recognition as sr
 from tempfile import NamedTemporaryFile
+from flask_cors import CORS
 
 # Load the Whisper model:
 model = whisper.load_model('base')
 
 app = Flask(__name__)
 app.secret_key = "caircocoders-ednalan"
+CORS(app)
 
 # UPLOAD_FOLDER = 'static/uploads'
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -25,9 +27,11 @@ ALLOWED_EXTENSIONS = set(['mp3', 'mpeg', 'wav'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def main():
     return "Is this Working ?"
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -42,9 +46,9 @@ def upload():
             return "Filename cant be empty"
 
         print("Form Data Received !!")
-        if file :
+        if file:
             uploads_dict = request.files.to_dict()
-            print("Items : ",uploads_dict.items())
+            print("Items : ", uploads_dict.items())
 
             for fileName, fileStorage in uploads_dict.items():
                 temp = NamedTemporaryFile()
@@ -59,7 +63,7 @@ def upload():
         #         data = recognizer.record(source)
         #     transcript = recognizer.recognize_google(data, key=None)
 
-    return transcripts
+    return transcript
 
 
 @app.route('/timestamps', methods=['POST'])
@@ -80,14 +84,14 @@ def getTimestamps():
 
         if 'search_word' in req_data:
             search_word = req_data['search_word']
-          
+
     trans_segs = transcript_data["segments"]
     timestamps = []
 
     for i in trans_segs:
         if search_word in i['text']:
-            start_time = round(i['start'],2)
-            end_time = round(i['end'],2)
+            start_time = round(i['start'], 2)
+            end_time = round(i['end'], 2)
             timestamps.append([start_time, end_time])
 
     return timestamps
